e235e45e6574b54e7bda9ee6393cad8a6d8896d4
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-01-24 16:18:26 +0530

    image path changed

--- script/animation.js
+++ script/animation.js
@@ -12,10 +12,10 @@ var isMobile = window.innerWidth < 540;
 
 // get image url for the section with frame index
 const getImageUrl = (section, index) => {
-  if (section == 4) return `https://store-o257sk57z9.mybigcommerce.com/content/Animation622/assets/images/${isMobile ? 'Mobile/':''}Sequence_01/sh_010${isMobile ? '_m':''}.00001.png`;
+  if (section == 4) return `https://ayatacommerce-ecommerce.github.io/nextbase/assets/images/${isMobile ? 'Mobile/':''}Sequence_01/sh_010${isMobile ? '_m':''}.00001.png`;
   if (section > 4) section = section - 1
 
-  return `https://store-o257sk57z9.mybigcommerce.com/content/Animation622/assets/images/${isMobile ? 'Mobile/':''}Sequence_${section.toString().padStart(2, "0")}/sh_${section
+  return `https://ayatacommerce-ecommerce.github.io/nextbase/assets/images/${isMobile ? 'Mobile/':''}Sequence_${section.toString().padStart(2, "0")}/sh_${section
     .toString().padStart(2, "0")}0${isMobile ? '_m':''}.${index.toString().padStart(5, "0")}.png`;
 }
 
